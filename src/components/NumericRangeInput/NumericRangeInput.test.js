5368616f9ca7a75a39352206aa7c519b34cc5865
Steve Robertson <SteveJRobertson@users.noreply.github.com>
2019-10-29 13:32:24 +0000

    Updated tests

--- src/components/NumericRangeInput/NumericRangeInput.test.js
+++ src/components/NumericRangeInput/NumericRangeInput.test.js
@@ -1,24 +1,33 @@
 import React from "react";
+import { act } from "react-dom/test-utils";
 import { mount } from "enzyme";
 import { ControlGroup, HTMLSelect, NumericInput } from "@blueprintjs/core";
 import setupTests from "../../../setupTests";
 import NumericRangeInput from "./NumericRangeInput";
 
 describe("NumericRangeInput", () => {
+  const onRangeInputChangeMock = jest.fn(() => {});
+
   let component;
   let controlGroup;
   let htmlSelect;
   let numericInput;
 
   beforeEach(() => {
-    const onRangeInputChangeMock = jest.fn(() => {});
+    act(() => {
+      component = mount(
+        <NumericRangeInput onRangeInputChange={onRangeInputChangeMock} />
+      );
+    });
 
-    component = mount(
-      <NumericRangeInput onRangeInputChange={onRangeInputChangeMock} />
-    );
+    onRangeInputChangeMock.mockReset();
     controlGroup = component.find(ControlGroup);
     htmlSelect = controlGroup.find(HTMLSelect);
     numericInput = controlGroup.find(NumericInput);
+  });
+
+  afterEach(() => {
+    component.unmount();
   });
 
   it("renders without crashing", () => {
@@ -26,7 +35,7 @@ describe("NumericRangeInput", () => {
   });
 
   it("renders a BlueprintJS ControlGroup", () => {
-    // TODO: Second ControlGroup exists in NumericInput component. Need a more robust test here.
+    // TODO: Second ControlGroup component exists inside NumericInput component. Need a more robust test here.
     expect(controlGroup).toHaveLength(2);
   });
 
@@ -36,5 +45,25 @@ describe("NumericRangeInput", () => {
 
   it("renders a BlueprintJS NumericInput inside the ControlGroup", () => {
     expect(numericInput).toHaveLength(1);
+  });
+
+  describe("when entering a number", () => {
+    it("calls onRangeInputChange with the updated value", () => {
+      const inputProps = numericInput.props();
+      act(() => {
+        inputProps.onValueChange(5, "5");
+      });
+      expect(onRangeInputChangeMock).toHaveBeenCalledWith("<", "5");
+    });
+  });
+
+  describe("when changing the range type", () => {
+    it.only("calls onRangeInputChange with the updated value", () => {
+      const selectProps = htmlSelect.props();
+      act(() => {
+        selectProps.onChange({ currentTarget: { value: "=" } });
+      });
+      expect(onRangeInputChangeMock).toHaveBeenCalledWith("=", null);
+    });
   });
 });
